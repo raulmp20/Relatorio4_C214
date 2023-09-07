00e342312ae966f93300f3ceb75c6d634c0822cf
raulmp20 <raulmp02@hotmail.com>
2023-09-07 15:36:52 -0300

    :recycle: Add aula4

---
+++ aula4/src/Jogador.svelte
@@ -0,0 +1,24 @@
+<script>
+    import "animate.css";
+
+    export let acao;
+
+</script>
+
+<style>
+  .botao-custom {
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+  }
+
+  .botao-custom:hover {
+    animation: heartBeat 1s infinite; 
+  }
+</style>
+
+<div>
+  <button on:click={acao} class="botao-custom">Marcar GOL</button>
+</div>
---
+++ aula4/src/Jogo.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Jogador from "./Jogador.svelte";
+	
+	function marcaGol(){
+		console.log("Marcou 1 GOL");
+	}
+	
+</script>
+
+<main>
+	<h1>Exibe a informação quando marca GOL</h1>
+	<Jogador acao = {marcaGol} />
+</main>
+
